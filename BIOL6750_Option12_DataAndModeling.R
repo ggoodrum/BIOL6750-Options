5b50193a8d9a46a25e8d6fb96cf0036628038d98
GGoodrum <goodrum.greg@gmail.com>
2018-11-28 13:58:43 -0700

    Updates to Option 12

--- BIOL6750_Option12_DataAndModeling.R
+++ BIOL6750_Option12_DataAndModeling.R
@@ -7,7 +7,7 @@
 
 library(data.table)
 airports <- fread("~/USU/Coursework/Programming for Biologist (BIOL 6750)/Code_Share/BIOL6750-Options/airports.csv")
-flightdata <- fread('~/Downloads/2008.csv')
+flightdata <- fread('~/Downloads/2008.csv', stringsAsFactors = FALSE)
 
 # 1. (b) Calculate the mean delay for each departure and arrival airport.
 #        Rank the airports by departure/arrival delays.
@@ -23,14 +23,35 @@ Ranked.Mean.Dep.Delay = setorder(Mean.Dep.Delay, -V1)
 
 # Can rename columns using colnames
 colnames(Ranked.Mean.Arr.Delay)[1] <- 'Airport'
+colnames(Ranked.Mean.Arr.Delay)[2] <- 'AvgArrDly(min)'
+colnames(Ranked.Mean.Dep.Delay)[1] <- 'Airport'
+colnames(Ranked.Mean.Dep.Delay)[2] <- 'AvgDepDly(min)'
 
-# Combine averages into a single table, then ready for use in cor.test
+# Combine averages into a single table based on shared field
+# cor.test requires lists of the same length, so this maintains
+# only the records that have both arrival and depature info
+
+AllDelays <- merge(Ranked.Mean.Arr.Delay,Ranked.Mean.Dep.Delay, by = 'Airport')
+
+# Rank AllDelays by either category
+
+AllDelay.Rank.Dep <- setorder(AllDelays, -'AvgDepDly(min)')
+AllDelay.Rank.Arr <- setorder(AllDelays, -'AvgArrDly(min)')
 
 
 # 1. (c) Use cor.test to see if there is a correlation between the arrival and
 #        departure delays for each airport.  Work from the summarized data
 #        calculated above, not the raw data.
 
+cor.test(AllDelays$`AvgDepDly(min)`, AllDelays$`AvgArrDly(min)`)
+
+# The test returns a correlation coefficient of 0.781582, which indicates a strong
+# positive linear relationship between arrival delay and departure delay.
+
 
 # 2. Use expand.grid, prog.bar, and seq to improve code.  Fix the lack of replication
 #    and address conflation of effect size with number of samples in their power analysis.
+
+
+
+# ---- Below this line is reference code from the handout ----
